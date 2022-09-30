83752ff68b1b7021de5a9c507c6de483f6ddd64d
Mehroj <mehrojgaybullayev49@gmail.com>
2022-09-30 22:23:55 +0500

    02

--- bin/basic_func02.dart
+++ bin/basic_func02.dart
@@ -1,8 +1,8 @@
 // Create a function called divide. 
 //Create function arguments a and b of type int. 
 //Returns the floor division of a and b.
-double divide(int a, int b) {
-    return a / b;
+int divide(int a, int b) {
+    return a ~/ b;
 }
 void main() {
     print(divide(9,3));
